4732a468fa036618a60ef17b1270d9726d52b6c1
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-03-30 15:03:21 +0300

    submit progress

--- Lesson/js/script.js
+++ Lesson/js/script.js
@@ -1,42 +1,62 @@
-// let form = document.querySelector(".form");
-// let nameInput = document.querySelector(".inputName");
-// let lastNameInput = document.querySelector(".inputLastName");
-// let age = document.querySelector("#age");
-// let male = document.getElementsByName("radio");
+// localStorage.setItem("name", "Kristina");
+
+// const a = localStorage.getItem("name");
+
+// console.log(a);
+
+// function check() {
+//     let val = document.getElementsByName("radio");
+//     if(val[0].checked) {
+//         console.log("Жіноча");
+//     } else if(val[1].checked) {
+//         console.log("Чоловіча");
+//     }
+// };
+
+// const form = document.querySelector(".form");
+
+// const nameInput = document.querySelector(".inputName");
+// const lastNameInput = document.querySelector(".inputLastName");
+// const age = document.querySelector("#age");
+// const male = document.getElementsByName("radio");
 
 // form.addEventListener("submit", handleSubmit);
 
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const info = {
-//     name: nameInput.value,
-//     lastName: lastNameInput.value,
-//     age: age.value,
-//     male: male[0].checked,
-//   };
-//   console.log(info);
-//   localStorage.setItem("info", JSON.stringify(info));
-// }
+// function handleSubmit(eventHandler) {
+//     eventHandler.preventDefault();
+//     const info = {
+//         name: nameInput.value,
+//         lastName: lastNameInput.value,
+//         age: age.value,
+//         male: male[1].checked,
+//     };
 
-// if (localStorage.getItem("info")) {
-//   const ls = JSON.parse(localStorage.getItem("info"));
-//   console.log(ls);
-//   nameInput.value = ls.name;
-//   lastNameInput.value = ls.lastName;
+//     localStorage.setItem("info", JSON.stringify(info));
+// }
+ 
+// if(localStorage.getItem("info")) {
+//     const ls = JSON.parse(localStorage.getItem("info"));
+//     console.log(ls);
+//     nameInput.value = ls.name;
+//     lastNameInput.value = ls.lastName;
+//     male[0].value = ls.male;
+//     age.value = ls.age;
 // }
 
 // let form = document.querySelector("#form");
 // let formData = {};
 
-// form.addEventListener("input", function (event) {
-//   formData[event.target.name] = event.target.value;
-//   localStorage.setItem("formData", JSON.stringify(formData));
+// form.addEventListener("input", function(eventHandler) {
+//     formData[eventHandler.target.name] = eventHandler.target.value;
+
+//     localStorage.setItem("formData", JSON.stringify(formData));
 // });
 
-// if (localStorage.getItem("formData")) {
-//   formData = JSON.parse(localStorage.getItem("formData"));
 
-//   for (let key in formData) {
-//     form.elements[key].value = formData[key];
-//   }
-// }
+// if(localStorage.getItem("formData")) {
+//     formData = JSON.parse(localStorage.getItem("formData"));
+
+//     for(let key in formData) {
+//         form.elements[key].value = formData[key];
+//     }
+// };
